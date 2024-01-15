b161cb1d2e16102fa63f0f0ecfff8473df663ac7
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2024-01-15 17:22:01 +0100

    test: add JSON converters tests

---
+++ tests/Play.Contracts.Tests/Json/PlayRequestJsonConverterTests.cs
@@ -0,0 +1,115 @@
+﻿using Bogus;
+using Play.Contracts.Json;
+using Play.Contracts.Requests;
+using Play.Contracts.Requests.Abstract;
+using Play.Contracts.Requests.Payloads;
+using Play.Contracts.Requests.Payloads.Primitives;
+
+namespace Play.Contracts.Tests.Json;
+
+public class PlayRequestJsonConverterTests
+{
+    private readonly Faker _faker
+        = new();
+    private readonly IPlayJson _json
+        = new PlayJson().WithPlayRequestJsonConverter();
+
+    [Fact]
+    public void LoginDevice_Serialize_ReturnExpectedJson()
+    {
+        var loginDevice = new LoginDevice(
+            _faker.Random.String2(
+                _faker.Random.Byte(8, 24)));
+        var expectedJson = Json(loginDevice);
+        
+        var json = _json.Serialize(loginDevice);
+        
+        Assert.Equal(json, expectedJson);
+    }
+    
+    [Fact]
+    public void LoginDevice_Deserialize_ReturnExpectedContract()
+    {
+        var expectedLoginDevice = new LoginDevice(
+            _faker.Random.String2(
+                _faker.Random.Byte(8, 24)));
+        var json = Json(expectedLoginDevice);
+        
+        var loginDevice = _json.Deserialize<IRequestWrapper<IRequestPayload>>(json);
+        
+        Assert.IsType<LoginDevice>(loginDevice);
+        Assert.IsType<LoginDevicePayload>(loginDevice.Payload);
+        Assert.Equal(LoginDevice.TypeValue, loginDevice.Type);
+        Assert.Equal(expectedLoginDevice.Payload, loginDevice.Payload);
+    }
+    
+    [Fact]
+    public void SendGift_Serialize_ReturnExpectedJson()
+    {
+        var sendGift = new SendGift(
+            recipientPlayerId: _faker.Random.Long(),
+            type: _faker.PickRandom<ResourceType>(),
+            value: _faker.Random.Int());
+        var expectedJson = Json(sendGift);
+        
+        var json = _json.Serialize(sendGift);
+        
+        Assert.Equal(json, expectedJson, ignoreCase: true);
+    }
+    
+    [Fact]
+    public void SendGift_Deserialize_ReturnExpectedContract()
+    {
+        var expectedSendGift = new SendGift(
+            recipientPlayerId: _faker.Random.Long(),
+            type: _faker.PickRandom<ResourceType>(),
+            value: _faker.Random.Int());
+        var json = Json(expectedSendGift);
+        
+        var sendGift = _json.Deserialize<IRequestWrapper<IRequestPayload>>(json);
+        
+        Assert.IsType<SendGift>(sendGift);
+        Assert.IsType<SendGiftPayload>(sendGift.Payload);
+        Assert.Equal(SendGift.TypeValue, sendGift.Type);
+        Assert.Equal(expectedSendGift.Payload, sendGift.Payload);
+    }
+    
+    [Fact]
+    public void UpdateResource_Serialize_ReturnExpectedJson()
+    {
+        var updateResource = new UpdateResource(
+            type: _faker.PickRandom<ResourceType>(),
+            value: _faker.Random.Int());
+        var expectedJson = Json(updateResource);
+        
+        var json = _json.Serialize(updateResource);
+        
+        Assert.Equal(json, expectedJson, ignoreCase: true);
+    }
+    
+    [Fact]
+    public void UpdateResource_Deserialize_ReturnExpectedContract()
+    {
+        var expectedUpdateResource = new UpdateResource(
+            type: _faker.PickRandom<ResourceType>(),
+            value: _faker.Random.Int());
+        var json = Json(expectedUpdateResource);
+        
+        var updateResource = _json.Deserialize<IRequestWrapper<IRequestPayload>>(json);
+        
+        Assert.IsType<UpdateResource>(updateResource);
+        Assert.IsType<UpdateResourcePayload>(updateResource.Payload);
+        Assert.Equal(UpdateResource.TypeValue, updateResource.Type);
+        Assert.Equal(expectedUpdateResource.Payload, updateResource.Payload);
+    }
+    
+    private static string Json(LoginDevice loginDevice)
+        => $"{{\"type\":\"{LoginDevice.TypeValue}\",\"payload\":{{\"currentDeviceUDID\":\"{loginDevice.Payload.CurrentDeviceUDID}\"}}}}";
+
+    private static string Json(SendGift sendGift)
+        => $"{{\"type\":\"{SendGift.TypeValue}\",\"payload\":{{\"recipientPlayerId\":{sendGift.Payload.RecipientPlayerId},\"type\":\"{sendGift.Payload.Type:G}\",\"value\":{sendGift.Payload.Value}}}}}";
+
+    private static string Json(UpdateResource updateResource)
+        => $"{{\"type\":\"{UpdateResource.TypeValue}\",\"payload\":{{\"type\":\"{updateResource.Payload.Type:G}\",\"value\":{updateResource.Payload.Value}}}}}";
+
+}
---
+++ tests/Play.Contracts.Tests/Json/PlayResponseJsonConverterTests.cs
@@ -0,0 +1,110 @@
+﻿using Bogus;
+using Play.Contracts.Json;
+using Play.Contracts.Responses;
+using Play.Contracts.Responses.Abstract;
+using Play.Contracts.Responses.Payloads;
+using Play.Contracts.Responses.Payloads.Bad;
+using Play.Contracts.Responses.Payloads.Ok;
+
+namespace Play.Contracts.Tests.Json;
+
+public class PlayResponseJsonConverterTests
+{
+    private readonly Faker _faker
+        = new();
+    private readonly IPlayJson _json
+        = new PlayJson().WithPlayResponseJsonConverter();
+
+    [Fact]
+    public void DeviceJoined_Serialize_ReturnExpectedJson()
+    {
+        var deviceJoined = new DeviceJoined(
+            playerId: _faker.Random.Long());
+        var expectedJson = Json(deviceJoined);
+        
+        var json = _json.Serialize(deviceJoined);
+        
+        Assert.Equal(json, expectedJson);
+    }
+    
+    [Fact]
+    public void DeviceJoined_Deserialize_ReturnExpectedContract()
+    {
+        var expectedDeviceJoined = new DeviceJoined(
+            playerId: _faker.Random.Long());
+        var json = Json(expectedDeviceJoined);
+        
+        var deviceJoined = _json.Deserialize<IResponseWrapper<IResponsePayload>>(json);
+        
+        Assert.IsType<DeviceJoined>(deviceJoined);
+        Assert.IsType<PlayerPayload>(deviceJoined.Payload);
+        Assert.Equal(DeviceJoined.TypeValue, deviceJoined.Type);
+        Assert.Equal(expectedDeviceJoined.Payload, deviceJoined.Payload);
+    }
+    
+    [Fact]
+    public void DeviceNotFound_Serialize_ReturnExpectedJson()
+    {
+        var deviceNotFound = new DeviceNotFound(
+            notFoundDeviceUDID: _faker.System.AndroidId());
+        var expectedJson = Json(deviceNotFound);
+        
+        var json = _json.Serialize(deviceNotFound);
+        
+        Assert.Equal(json, expectedJson, ignoreCase: true);
+    }
+    
+    [Fact]
+    public void DeviceNotFound_Deserialize_ReturnExpectedContract()
+    {
+        var expectedDeviceNotFound = new DeviceNotFound(
+            notFoundDeviceUDID: _faker.System.AndroidId());
+        var json = Json(expectedDeviceNotFound);
+        
+        var deviceNotFound = _json.Deserialize<IResponseWrapper<IResponsePayload>>(json);
+        
+        Assert.IsType<DeviceNotFound>(deviceNotFound);
+        Assert.IsType<DeviceNotFoundPayload>(deviceNotFound.Payload);
+        Assert.Equal(DeviceNotFound.TypeValue, deviceNotFound.Type);
+        Assert.Equal(expectedDeviceNotFound.Payload, deviceNotFound.Payload);
+    }
+    
+    [Fact]
+    public void DeviceRejected_Serialize_ReturnExpectedJson()
+    {
+        var deviceRejected = new DeviceRejected(
+            alreadyConnectedDeviceUDID: _faker.System.AndroidId(),
+            playerId: _faker.Random.Long());
+        var expectedJson = Json(deviceRejected);
+        
+        var json = _json.Serialize(deviceRejected);
+        
+        Assert.Equal(json, expectedJson, ignoreCase: true);
+    }
+    
+    [Fact]
+    public void DeviceRejected_Deserialize_ReturnExpectedContract()
+    {
+        var expectedDeviceRejected = new DeviceRejected(
+            alreadyConnectedDeviceUDID: _faker.System.AndroidId(),
+            playerId: _faker.Random.Long());
+        var json = Json(expectedDeviceRejected);
+        
+        var deviceRejected = _json.Deserialize<IResponseWrapper<IResponsePayload>>(json);
+        
+        Assert.IsType<DeviceRejected>(deviceRejected);
+        Assert.IsType<DeviceRejectPayload>(deviceRejected.Payload);
+        Assert.Equal(DeviceRejected.TypeValue, deviceRejected.Type);
+        Assert.Equal(expectedDeviceRejected.Payload, deviceRejected.Payload);
+    }
+    
+    private static string Json(DeviceJoined deviceJoined)
+        => $"{{\"type\":\"{DeviceJoined.TypeValue}\",\"payload\":{{\"playerId\":{deviceJoined.Payload.PlayerId}}}}}";
+
+    private static string Json(DeviceNotFound deviceNotFound)
+        => $"{{\"type\":\"{DeviceNotFound.TypeValue}\",\"payload\":{{\"notFoundDeviceUDID\":\"{deviceNotFound.Payload.NotFoundDeviceUDID}\"}}}}";
+
+    private static string Json(DeviceRejected deviceRejected)
+        => $"{{\"type\":\"{DeviceRejected.TypeValue}\",\"payload\":{{\"AlreadyConnectedDeviceUDID\":\"{deviceRejected.Payload.AlreadyConnectedDeviceUDID}\",\"playerId\":{deviceRejected.Payload.PlayerId}}}}}";
+
+}
