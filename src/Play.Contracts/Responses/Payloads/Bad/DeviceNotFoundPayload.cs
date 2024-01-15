40cdbab9830dab79785135ba60261a127c05b872
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2024-01-15 15:27:44 +0100

    chore: add missing files of the commit #adb2d1a1

---
+++ src/Play.Contracts/Responses/Payloads/Bad/DeviceNotFoundPayload.cs
@@ -0,0 +1,6 @@
+﻿namespace Play.Contracts.Responses.Payloads.Bad;
+
+public record DeviceNotFoundPayload : IResponsePayload
+{
+    public string NotFoundDeviceUDID { get; init; } = string.Empty;
+}
---
+++ src/Play.Contracts/Responses/Payloads/Bad/DeviceRejectPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace Play.Contracts.Responses.Payloads.Bad;
+
+public record DeviceRejectPayload : IResponsePayload
+{
+    public string AlreadyConnectedDeviceUDID { get; init; } = string.Empty;
+    
+    public long PlayerId { get; init; }
+}
