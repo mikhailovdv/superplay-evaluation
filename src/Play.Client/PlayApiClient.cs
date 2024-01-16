1f223d7025fe71b442c3b1f34d039998883caaa1
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2024-01-16 03:09:15 +0100

    feat: add grubby test client

---
+++ src/Play.Client/PlayApiClient.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using Play.Contracts.Extensions;
+using Play.Contracts.Requests.Abstract;
+using Play.Contracts.Requests.Payloads;
+using Serilog;
+
+namespace Play.Client;
+
+public class PlayApiClient
+{
+    private readonly ClientWebSocket _clientWebSocket
+        = new();
+    private readonly CancellationTokenSource _cancellationTokenSource
+        = new();
+    
+    private readonly Uri _uri;
+
+    public PlayApiClient(string uri)
+        => _uri = new Uri(uri);
+
+    public Task ConnectAsync()
+        => _clientWebSocket.ConnectAsync(_uri, _cancellationTokenSource.Token);
+    
+    public Task DisconnectAsync()
+        => _clientWebSocket.CloseAsync(
+            WebSocketCloseStatus.NormalClosure,
+            "Closed",
+            _cancellationTokenSource.Token);
+
+    public Task SendAsync<TMessage>(
+        TMessage request)
+        where TMessage : IRequestWrapper<IRequestPayload>
+        => _clientWebSocket.SendAsync(request, _cancellationTokenSource.Token);
+    
+    public async Task ReceiveAsync()
+    {
+        Log.Information("Receiving ...");
+        
+        var buffer = new byte[4096];
+        await _clientWebSocket.ReceiveAsync(
+            new ArraySegment<byte>(buffer),
+            _cancellationTokenSource.Token);
+        
+        Log.Information("Received");
+        
+        var json = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+        
+        Log.Information("Got {@Json}", json);
+    }
+}
--- src/Play.Client/Program.cs
+++ src/Play.Client/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Play.Client;
+using Play.Contracts.Requests;
+using Play.Contracts.Requests.Abstract;
+using Play.Contracts.Requests.Payloads;
+using Play.Contracts.Requests.Payloads.Primitives;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+const string apiHost = "wss://localhost:7001/play";
+
+var playApiClient = new PlayApiClient(apiHost);
+
+await playApiClient.ConnectAsync();
+
+await Task.Factory.StartNew(async () =>
+{
+    await playApiClient.ReceiveAsync();
+}, TaskCreationOptions.LongRunning);
+
+const string existingDeviceUDID = "device-1";
+var loginDevice = new LoginDevice(
+    currentDeviceUDID: existingDeviceUDID);
+
+await playApiClient.SendAsync(loginDevice);
+
+await Task.Delay(5000);
+
+while (!Console.KeyAvailable) {
+    var sendGift = new SendGift(
+        1,
+        ResourceType.Coin,
+        100);
+    await playApiClient.SendAsync(sendGift);
+    Log.Information("Sent {@SendGift} ...", sendGift);
+    await Task.Delay(5000); 
+}
+
+Console.WriteLine("Press any key to exit");
+
+Console.ReadKey();
+
+Console.WriteLine("Disconnecting from {0} ...", apiHost);
+await playApiClient.DisconnectAsync();
+Console.WriteLine("Disconnected");
+
+Console.WriteLine("Gone");
