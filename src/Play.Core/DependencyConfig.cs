a362a4220cb6c0a1c0c0e8de98b56784926e008f
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2024-01-16 02:37:39 +0100

    feat: add play service dependency injection configuration

---
+++ src/Play.Core/DependencyConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Play.Core;
+
+public static class DependencyConfig
+{
+    public static IServiceCollection AddServices(
+        this IServiceCollection services)
+    {
+        services.AddTransient<IPlayService, PlayService>();
+        return services;
+    }
+    
+}
